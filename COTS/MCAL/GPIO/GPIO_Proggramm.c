03f1d1154ef1f22ef21a95cd28d4af3cad835267
dizzydroid <shehabmahmoud2003@gmail.com>
2024-04-27 17:06:59 +0300

    added GPIO_Program enhancements

--- COTS/MCAL/GPIO/GPIO_Proggramm.c
+++ COTS/MCAL/GPIO/GPIO_Proggramm.c
@@ -1,9 +1,9 @@
-/******************************************************************/
+/*******************************************************************/
 /*************   Author: ASU_EMBEDDED_TEAM_NO(?)      **************/
-/*************        date:  27 April 2024           **************/
-/*************           Version: 0.1                **************/
-/*************    Module:MCAL_GPIO_proggramm.c       **************/
-/*****************************************************************/
+/*************        date:  27 April 2024            **************/
+/*************           Version: 0.1                 **************/
+/*************    Module: MCAL_GPIO_proggramm.c       **************/
+/*******************************************************************/
 
 /*****************************< LIB *****************************/
 #include "BIT_MATH.h"
@@ -18,12 +18,17 @@
 Std_ReturnType MCAL_GPIO_INIT(u32 Port, GPIO_REGISTERES *Copy_PortId)
 {
     Std_ReturnType Local_FunctionStatus = E_OK;
+    
+    GPIO_ENABLE |= 1 << Port;
 
-    GPIO_ENABLE |= 1 << Port; 
-    while ((GPIO_WAIT & 1 << Port) == 0)
-        ;
-    Copy_PortId->GPIOLOCK = LOCK_VALUE;
-    Copy_PortId->GPIOCR |= 1 << Port;
+    // Add a check to see if enabling the GPIO port was successful
+    if (!(GPIO_ENABLE & (1 << Port))) {
+        Local_FunctionStatus = E_NOT_OK;
+    } else {
+    while ((GPIO_WAIT & 1 << Port) == 0); // Wait until port is ready
+    Copy_PortId->GPIOLOCK = LOCK_VALUE; // Unlock the port
+    Copy_PortId->GPIOCR |= 1 << Port; // Enable the port (set control register)
+    }
 
     return Local_FunctionStatus;
 }
