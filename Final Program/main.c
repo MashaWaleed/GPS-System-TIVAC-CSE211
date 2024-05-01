498c56aba18330f0f0da3ed5940991a3119e1eb0
Mohamed Waleed Elsayed <mw2969866@gmail.com>
2024-05-01 23:36:56 +0300

    Completed main.c

--- Final Program/main.c
+++ Final Program/main.c
@@ -1,11 +1,33 @@
+/*Driver Includes*/
+#include "UART_int.h"
+#include "GPS.h"
+#include "APP.h"
+#include "GPIO_Interface.h"
+#include "GPIO_Private.h"
+
+/*Struct holding UART Configurations for our custom UART Driver*/
+UART_ConfigType cfg;
+
+/*Global Variables*/
+float current_latitude = 0;
+float current_longitude = 0;
+
+float first_latitude = 0;
+float first_longitude = 0;
+
+float distance = 0;
+
+
+
 void UART2_INIT()
 {
-	// Initialising the UART instance that communicated with GPS system
+	// Initialising the UART instance that communicates with GPS system
 	cfg.Instance = UART2;
 	cfg.BaudRate = 9600;
 	cfg.Data_Bit = Data_Bit_8;
 	cfg.Parity = Parity_None;
 	cfg.Stop_Bit = Stop_bit_1;
+	
 	MCAL_UART_Init(&cfg);
 }
 
@@ -23,3 +45,31 @@ void Distination_Reached()
 	MCAL_GPIO_SetPinValue(GPIO_PORTF,GPIO_PIN1,GPIO_HIGH);
 	while(1);
 }
+
+
+
+int main(){
+		
+		// 	Initializing Flashing LED and UART for GPS 
+		RED_led_Init();
+		UART2_INIT();
+		
+		//	Poll untill a valid first GPS reading is found
+		while((HAL_GPS_Read(&cfg,&first_latitude, &first_longitude)) == 0);
+
+    while(1){
+			
+			// Get the distance every GPS tick
+			distance = computeDistance(current_latitude,current_longitude, first_latitude,first_longitude);
+			while((HAL_GPS_Read(&cfg,&current_latitude, &current_longitude)) == 0);
+			
+			// If the distance reaches 100m, we stop reading and light LED
+			if(distance >= 100)
+			{
+					Distination_Reached();
+			}
+    }
+}
+
+
+
