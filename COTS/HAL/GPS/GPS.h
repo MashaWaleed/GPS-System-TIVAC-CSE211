bf407c1bfec788cc8a2bddbc14952fa91e537547
Ahmed Ashraf <ahmed.ashraf2827@gmail.com>
2024-05-16 20:41:10 +0300

    Updated GPS drivers

--- COTS/HAL/GPS/GPS.h
+++ COTS/HAL/GPS/GPS.h
@@ -3,11 +3,26 @@
 
 #include "stdint.h"
 
-#include "UART_prv.h"
-#include "UART_cfg.h"
-#include "UART_int.h"
-
+#include "../../MCAL/UART/UART_prv.h"
+#include "../../MCAL/UART/UART_cfg.h"
+#include "../../MCAL/UART/UART_int.h"
+/**
+ * @brief Parses GPS data from a NMEA sentence
+ * @param GPS_Sentence Pointer to the GPS sentence string
+ * @param cfg Pointer to UART configuration
+ * @param latitude Pointer to store latitude value
+ * @param longitude Pointer to store longitude value
+ * @return Returns 1 if parsing was successful, 0 otherwise
+ */
 uint8_t parse_GPS_data(char* GPS_Sentence,UART_ConfigType * cfg, float * latitude, float * longitude);
+
+/**
+ * @brief Reads GPS data from UART
+ * @param cfg Pointer to UART configuration
+ * @param latitude Pointer to store latitude value
+ * @param longitude Pointer to store longitude value
+ * @return Returns 1 if GPS data was successfully read and parsed, 0 otherwise
+ */
 uint8_t HAL_GPS_Read(UART_ConfigType * cfg, float * latitude, float * longitude);
 
 
